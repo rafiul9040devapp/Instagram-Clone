b2704858c0fb8a7c5244c2cfab7a938554f89043
MD.Rafiul Haque <rafiulbinoy@gmail.com>
2024-03-07 12:20:50 +0600

    need to start the work with search

--- app/src/main/java/com/rafiul/instagramclone/adapters/PostAdapter.kt
+++ app/src/main/java/com/rafiul/instagramclone/adapters/PostAdapter.kt
@@ -10,63 +10,62 @@ import com.github.marlonlom.utilities.timeago.TimeAgo
 import com.rafiul.instagramclone.R
 import com.rafiul.instagramclone.databinding.ViewHolderAllPostBinding
 import com.rafiul.instagramclone.models.Post
-import com.squareup.picasso.Picasso
 
-class PostAdapter(val context: Context, private val postList: ArrayList<Post>) : RecyclerView.Adapter<PostAdapter.ViewHolder>() {
+class PostAdapter(private val context: Context, private val postList: List<Post>) : RecyclerView.Adapter<PostAdapter.ViewHolder>() {
 
-    inner class ViewHolder(val binding: ViewHolderAllPostBinding) : RecyclerView.ViewHolder(binding.root){
-        var isButtonPressed = false
+    inner class ViewHolder(private val binding: ViewHolderAllPostBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        private var isButtonPressed = false
+        fun bindWithView(post: Post) {
+            with(binding) {
+                Glide.with(context)
+                    .load(post.image)
+                    .centerCrop()
+                    .placeholder(R.drawable.user)
+                    .into(imageViewProfile)
+
+                Glide.with(context)
+                    .load(post.postUrl)
+                    .centerCrop()
+                    .placeholder(R.drawable.loading)
+                    .into(imageViewPost)
+
+                tVUserName.text = post.name
+                textViewCaption.text = post.caption
+
+                if (post.time.isNotEmpty()) {
+                    textViewPostTime.text = TimeAgo.using(post.time.toLong())
+                } else {
+                    textViewPostTime.text = post.time
+                }
+
+                imageViewLike.setOnClickListener {
+                    isButtonPressed = !isButtonPressed
+                    if (isButtonPressed) {
+                        imageViewLike.setBackgroundResource(R.drawable.heart_filled)
+                    } else {
+                        imageViewLike.setBackgroundResource(R.drawable.heart)
+                    }
+                }
+
+                imageViewShare.setOnClickListener {
+                    val shareIntent = Intent(Intent.ACTION_SEND)
+                    shareIntent.type = "text/plain"
+                    shareIntent.putExtra(Intent.EXTRA_TEXT, post.postUrl)
+                    context.startActivity(shareIntent)
+                }
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(ViewHolderAllPostBinding.inflate(LayoutInflater.from(context),parent,false))
+        val binding = ViewHolderAllPostBinding.inflate(LayoutInflater.from(context), parent, false)
+        return ViewHolder(binding)
     }
 
-    override fun getItemCount(): Int {
-       return postList.size
-    }
+    override fun getItemCount(): Int = postList.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-       val post = postList[position]
-        holder.binding.apply {
-            Glide
-                .with(context)
-                .load(post.image)
-                .centerCrop()
-                .placeholder(R.drawable.user)
-                .into(imageViewProfile)
-
-            Glide
-                .with(context)
-                .load(post.postUrl)
-                .centerCrop()
-                .placeholder(R.drawable.loading)
-                .into(imageViewPost)
-
-            tVUserName.text = post.name
-            textViewCaption.text = post.caption
-
-            if (post.time != ""){
-                textViewPostTime.text = TimeAgo.using(post.time.toLong())
-            }else{
-                textViewPostTime.text = post.time
-            }
-
-            imageViewLike.setOnClickListener {
-                holder.isButtonPressed = !holder.isButtonPressed
-                if (holder.isButtonPressed) {
-                   imageViewLike.setBackgroundResource(R.drawable.heart_filled)
-                } else {
-                    imageViewLike.setBackgroundResource(R.drawable.heart)
-                }
-            }
-
-            imageViewShare.setOnClickListener {
-                val shareIntent = Intent(Intent.ACTION_SEND)
-                shareIntent.type = "text/plain"
-                shareIntent.putExtra(Intent.EXTRA_TEXT,post.postUrl)
-                context.startActivity(shareIntent)
-            }
-        }
+        holder.bindWithView(postList[position])
     }
 }
